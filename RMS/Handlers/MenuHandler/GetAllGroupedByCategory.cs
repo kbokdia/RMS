437f1e6e3ad7786fc9d1517782ee541c69718c11
Pravin Raj <pravinraj@mwm.se>
2022-06-11 20:17:58 +0530

    Changes of Menu Controller
    Menu controller get method which returns data grouped by categories

---
+++ RMS/Handlers/MenuHandler/GetAllGroupedByCategory.cs
@@ -0,0 +1,46 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using RMS.Data;
+using RMS.Models;
+using System.Threading;
+using System.Threading.Tasks;
+using RMS.Exceptions;
+using System.Linq;
+
+namespace RMS.Handlers.MenuHandler
+{
+   public class GetAllGroupedByCategory : IRequestHandler<GetAllGroupedByCategory.Request, GetAllGroupedByCategory.Response>
+   {
+      IMediator mediator;
+      public GetAllGroupedByCategory(IMediator mediator) {
+         this.mediator = mediator;
+      }
+      public class Request : IRequest<Response>
+      {
+         public string Name { get; set; } = null;
+      }
+      public class Response
+      {
+         public string Message { get; set; }
+         public object Data { get; set; }
+      }
+      public async Task<Response> Handle(GetAllGroupedByCategory.Request request, CancellationToken cancellationToken)
+      {
+
+         var getItemRequest = new GetAll.GetAllMenuRequest() { Name = request.Name };
+         var menuItems = await mediator.Send(getItemRequest);
+         // var groupedItems = menuItems.Data.GroupBy(x => x.CategoryType, x=> x);
+         var groupedItems = menuItems.Data
+            .GroupBy(x => x.CategoryType, x => x)
+            .Select(x => new { category = x?.Key, items = x?.Select(y => y).ToList() });
+         // var groupedItemArray = groupedItems.Select(x => new { category = x.Key, items = x[x.Key] })
+
+
+         return new Response
+         {
+            Message = "All OK.",
+            Data = groupedItems
+         };
+      }
+   }
+}
---
+++ RMS/Models/MenuModel.cs
@@ -0,0 +1,80 @@
+﻿using RMS.Entities.Scaffold;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+
+namespace RMS.Models
+{
+   public class CreateMenuModel
+   {
+
+      [StringLength(75)]
+      public string Name { get; set; }
+
+      [StringLength(75)]
+      public string CategoryType { get; set; }
+
+      public double? Price { get; set; }
+
+      [StringLength(500)]
+      public string Description { get; set; }
+
+      [StringLength(500)]
+      public string ImageUrl { get; set; }
+
+      [StringLength(500)]
+      public string Tags { get; set; }
+      public string[] TagsList { get; set; }
+
+      public bool IsVeg { get; set; }
+      public Status Status { get; set; }
+
+      public static T ToModel<T>(Menu entity) where T : CreateMenuModel, new()
+      {
+         var model = new T();
+         model.Name = entity.Name;
+         model.CategoryType = entity.CategoryType;
+         model.Price = entity.Price;
+         model.Description = entity.Description;
+         model.ImageUrl = entity.ImageUrl;
+         model.IsVeg = entity.IsVeg <= 1;
+         model.Status = (Status)entity.Status;
+         model.Tags = entity.Tags;
+         model.TagsList = entity.Tags?.Split(',')?
+            .Select(tag => tag.Trim())?.ToArray();
+
+         return model;
+      }
+
+      public static Menu ToEntity<T>(T model) where T : CreateMenuModel, new()
+      {
+         var entity = new Menu();
+         entity.Name = model.Name;
+         entity.CategoryType = model.CategoryType;
+         entity.Price = model.Price;
+         entity.Description = model.Description;
+         entity.ImageUrl = model.ImageUrl;
+         entity.IsVeg = (byte)(model.IsVeg ? 1 : 2);
+         entity.Status = (byte)model.Status;
+         entity.Tags = string.Join(",", model.TagsList);
+         return entity;
+      }
+
+   }
+   public class MenuModel : CreateMenuModel
+   {
+      public int Id { get; set; }
+      public new static T ToModel<T>(Menu entity) where T : MenuModel, new()
+      {
+         var model = CreateMenuModel.ToModel<T>(entity);
+         model.Id = entity.Id;
+         return model;
+      }
+
+      public new static Menu ToEntity<T>(T model) where T : MenuModel, new()
+      {
+         var entity = CreateMenuModel.ToEntity(model);
+         entity.Id = entity.Id;
+         return entity;
+      }
+   }
+}
