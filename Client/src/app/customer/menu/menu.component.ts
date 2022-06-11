94fb6c276b2de1ae3fd036c385e26d7104801246
Pooja Bokdia <poojabokdia1995@gmail.com>
2022-06-11 19:01:50 +0530

    Navbar and Cart (#6)

--- Client/src/app/customer/menu/menu.component.ts
+++ Client/src/app/customer/menu/menu.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-menu',
@@ -8,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class MenuComponent implements OnInit {
   panelOpenState = false;
   quantity:number = 1;
+  @Input() alwaysExpanded:boolean = false;
+  @Input() title:string = 'MENU';
   constructor() { 
   }
 
